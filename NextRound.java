b024ed6062be40044afd51b0d232bce3ede54bf0
ssnehatiwari21 <sneha.24bce11287@vitbhopal.ac.in>
2026-07-22 17:15:00 +0530

    Add files via upload

---
+++ NextRound.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+
+        int[] a = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+
+        int qualifyingScore = a[k - 1];
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (a[i] >= qualifyingScore && a[i] > 0) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
