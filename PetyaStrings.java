4f84a881e1b3f85913b2d53329bef466ac48b314
ssnehatiwari21 <sneha.24bce11287@vitbhopal.ac.in>
2026-07-29 10:50:59 +0530

    Add files via upload

---
+++ PetyaStrings.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s1 = sc.next().toLowerCase();
+        String s2 = sc.next().toLowerCase();
+
+        int result = s1.compareTo(s2);
+
+        if (result < 0) {
+            System.out.println(-1);
+        } else if (result > 0) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+    }
+}
