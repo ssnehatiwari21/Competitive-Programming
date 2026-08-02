7ca8cf7318e68ee0a9dfabeee1fbec6246a19123
ssnehatiwari21 <sneha.24bce11287@vitbhopal.ac.in>
2026-08-03 00:02:08 +0530

    Add files via upload

---
+++ Capatalisation.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String word = sc.next();
+
+        if (word.length() > 0) {
+            word = Character.toUpperCase(word.charAt(0)) + word.substring(1);
+        }
+
+        System.out.println(word);
+    }
+}
