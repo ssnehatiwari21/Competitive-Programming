6ce812793c45ee21264d654ec56d96008e858cd4
ssnehatiwari21 <sneha.24bce11287@vitbhopal.ac.in>
2026-08-06 12:28:40 +0530

    Add files via upload

---
+++ StringTask.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+
+        StringBuilder ans = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = Character.toLowerCase(s.charAt(i));
+
+            if (ch == 'a' || ch == 'e' || ch == 'i' ||
+                ch == 'o' || ch == 'u' || ch == 'y') {
+                continue;
+            }
+
+            ans.append('.');
+            ans.append(ch);
+        }
+
+        System.out.println(ans);
+    }
+}
