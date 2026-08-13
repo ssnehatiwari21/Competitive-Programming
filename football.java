f97c392f90095c678972fee09a7d3aba0935a6d8
ssnehatiwari21 <sneha.24bce11287@vitbhopal.ac.in>
2026-08-13 18:11:04 +0530

    Add files via upload

---
+++ football.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.nextLine();
+
+        int count = 1;
+
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == s.charAt(i - 1)) {
+                count++;
+
+                if (count >= 7) {
+                    System.out.println("YES");
+                    return;
+                }
+            } else {
+                count = 1;
+            }
+        }
+
+        System.out.println("NO");
+    }
+}
