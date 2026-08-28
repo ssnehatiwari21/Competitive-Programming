d16b308f7c6ff5e9780712e7ce6cb9dd7845a7d1
ssnehatiwari21 <sneha.24bce11287@vitbhopal.ac.in>
2026-08-28 10:09:45 +0530

    Implement Odds and Evens logic in OddsEvens.java

---
+++ OddsEvens.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long n = sc.nextLong();
+        long k = sc.nextLong();
+
+        long odd = (n + 1) / 2;
+
+        if (k <= odd) {
+            System.out.println(2 * k - 1);
+        } else {
+            System.out.println(2 * (k - odd));
+        }
+    }
+}
